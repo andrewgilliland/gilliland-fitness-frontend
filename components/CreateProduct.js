6e6dfca863f9a092e1da58bfd32f2929908706c8
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-04-22 09:09:22 -0500

    refetch queries after mutation and change page after product creation

--- components/CreateProduct.js
+++ components/CreateProduct.js
@@ -1,8 +1,10 @@
+import Router from 'next/router';
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
 import FormStyles from './styles/FormStyles';
 import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
@@ -41,6 +43,7 @@ export default function CreateProduct() {
     CREATE_PRODUCT_MUTATION,
     {
       variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
     }
   );
 
@@ -48,10 +51,13 @@ export default function CreateProduct() {
     <FormStyles
       onSubmit={async (e) => {
         e.preventDefault();
-        console.log(inputs);
         // Submit the input fields to the backend:
-        await createProduct();
+        const res = await createProduct();
         clearForm();
+        // Go to that product's page!
+        Router.push({
+          pathname: `/products/${res.data.createProduct.id}`,
+        });
       }}
     >
       <DisplayError error={error} />
