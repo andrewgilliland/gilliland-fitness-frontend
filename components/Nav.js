946a07b528dff2f2d131313049efd37d9eac318c
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-05-03 13:29:58 -0500

    adds user component and signin page

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,26 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import { useUser } from './User';
 
 export default function Nav() {
+  const user = useUser();
+  console.log(user);
+
   return (
     <NavStyles>
       <Link href="/products">products</Link>
-      <Link href="/sell">sell</Link>
-      <Link href="/orders">orders</Link>
-      <Link href="/account">account</Link>
+      {user && (
+        <>
+          <Link href="/sell">sell</Link>
+          <Link href="/orders">orders</Link>
+          <Link href="/account">account</Link>
+        </>
+      )}
+      {!user && (
+        <>
+          <Link href="/signin">Sign In</Link>
+        </>
+      )}
     </NavStyles>
   );
 }
