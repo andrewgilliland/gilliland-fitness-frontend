64a69c713e9a98812bee4672409d70e0b4dbb28e
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-05-03 17:17:02 -0500

    adds signin component

--- components/ErrorMessage.js
+++ components/ErrorMessage.js
@@ -20,11 +20,15 @@ const ErrorStyles = styled.div`
 
 const DisplayError = ({ error }) => {
   if (!error || !error.message) return null;
-  if (error.networkError && error.networkError.result && error.networkError.result.errors.length) {
+  if (
+    error.networkError &&
+    error.networkError.result &&
+    error.networkError.result.errors.length
+  ) {
     return error.networkError.result.errors.map((error, i) => (
       <ErrorStyles key={i}>
         <p data-test="graphql-error">
-          <strong>Shoot!</strong>
+          <strong>Oh No!</strong>
           {error.message.replace('GraphQL error: ', '')}
         </p>
       </ErrorStyles>
@@ -33,7 +37,7 @@ const DisplayError = ({ error }) => {
   return (
     <ErrorStyles>
       <p data-test="graphql-error">
-        <strong>Shoot!</strong>
+        <strong>Oh No!</strong>
         {error.message.replace('GraphQL error: ', '')}
       </p>
     </ErrorStyles>
--- pages/signin.js
+++ pages/signin.js
@@ -1,7 +1,10 @@
+import SignIn from '../components/SignIn';
+
 export default function SignInPage() {
   return (
     <>
       <p>Sign In!</p>
+      <SignIn />
     </>
   );
 }
